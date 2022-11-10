8b10ba0ee12d40983a34f13567782e2e7854f8dc
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-10 23:16:24 +0100

    Add Jest and React testing library

---
+++ frontend/tests/react-testing-library/segments/segments.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Segments from "@/pages/segments";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Segments />);
+
+    const button = screen.getByRole("button", {
+      name: "Hello world",
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+});
