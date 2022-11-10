edbe0ea444368bd6286fd089617951316fd0cd73
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-10 09:57:50 +0100

    Add semgments Index page

--- frontend/pages/segments/index.tsx
+++ frontend/pages/segments/index.tsx
@@ -1,1 +1,10 @@
-export { Segments as default } from "./segments";
+import { Button } from "@mantine/core";
+
+const Segments = () => (
+  <div>
+    Segments page
+    <Button>Hello world</Button>
+  </div>
+);
+
+export default Segments;
--- frontend/pages/segments/segments.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@mantine/core";
-
-export const Segments = () => (
-  <div>
-    Segments page
-    <Button>Hello world</Button>
-  </div>
-);
