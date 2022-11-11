d416eed57f6a54f5f0a5df391fc3f0eeee5f93db
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-11 13:54:24 +0100

    Delete global styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
-import "../styles/globals.css";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider
