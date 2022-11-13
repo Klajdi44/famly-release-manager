541368686d4e1ac82b28f0f44a2b5460d0a0dae1
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-13 19:03:52 +0100

    Add userReducer

---
+++ frontend/pages/login/constants.ts
@@ -0,0 +1,7 @@
+import * as Types from "./types";
+
+const DOMAIN = "AUTH";
+
+const DEFAULT_STATE: Types.State = null;
+
+export { DOMAIN, DEFAULT_STATE };
---
+++ frontend/pages/segments/constants.ts
@@ -0,0 +1,3 @@
+const DOMAIN = "SEGMENTS";
+
+export { DOMAIN };
