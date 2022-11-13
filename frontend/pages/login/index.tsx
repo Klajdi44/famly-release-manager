6d2b203efb51fcfb3194b346a4a3dfede2cc5401
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-13 23:40:59 +0100

    Add user

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import {
+  TextInput,
+  PasswordInput,
+  Paper,
+  Title,
+  Container,
+  Button,
+} from "@mantine/core";
 
 const Login = () => {
-  return <div> Login page </div>;
+  return (
+    <Container size={420} my={40}>
+      <Title align="center">Famly RM</Title>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="Enter your email" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Enter your password"
+          required
+          mt="md"
+        />
+
+        <Button fullWidth mt="xl">
+          Log in
+        </Button>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
