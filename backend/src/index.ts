76dfada6695b91cad1e1cb9244bae2c18a34a270
Andrei Mihutoni <andrei.m@humanhotel.com>
2022-11-18 09:05:19 +0100

    adding http requests logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
-
-import { router as featureRouter } from "./routes/features";
+import { router as featuresRouter } from "./routes/features";
 
 dotenv.config();
 
+const logger = require ('morgan');
 const app = express();
-
+app.use(logger('dev'));
 app.use(express.json());
-
-app.use("/api/features", featureRouter);
+app.use("/api/features", featuresRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
