8d78cff1c4861176f4388de982f99a166c1f1b5d
Andrei Mihutoni <andrei.m@humanhotel.com>
2022-11-21 10:31:48 +0100

    adding JWT authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,24 @@
 import express from "express";
 import * as dotenv from "dotenv";
-
+import { generateToken } from './api/utils/jwt.utils';
 import { router as featureRouter } from "./routes/features";
+import * as Auth from '../src/middlewares/auth.middleware';
+
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
+// JWT authorize accessTypes
+app.use(Auth.authorize(['getAllFeatures']));
 app.use("/api/features", featureRouter);
 
+// Only generate a token for lower level environments
+if (process.env.NODE_ENV !== 'production') {
+  console.log('JWT', generateToken());
+}
+
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+  console.log(`### The app is listening at http://localhost:${process.env.PORT} ###`);
 });
--- backend/src/routes/features.ts
+++ backend/src/routes/features.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import { getAllFeatures } from "../controllers/featureController";
+import * as Auth from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
+// route without auth:
 router.get("/", getAllFeatures);
+
+// route with JWT auth:
+// router.get("/", Auth.authorize(['getAllFeatures']));
 
 export { router };
