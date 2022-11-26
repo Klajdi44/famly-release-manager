8b68bc2d26c115dc92d1adefcd00dbbdd60e6a75
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-26 20:29:29 +0100

    Add fix to docker

--- frontend/hooks/use-global-state/use-global-state.ts
+++ frontend/hooks/use-global-state/use-global-state.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { Context } from "./context";
+import { Context } from "../../global-state/context";
 
 const useGlobalState = () => useContext(Context);
 
