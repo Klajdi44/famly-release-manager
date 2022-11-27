76ffaedef246f61491dff5c79855d19e43d05c86
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-27 23:03:31 +0100

    Make react library work and add test

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,4 +2,6 @@ export default {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
 };
---
+++ frontend/src/tests/react-testing-library/segments/login.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Login from "../../../pages/login/login";
+
+describe("Login page", () => {
+  it("should render login page, button should be disabled if inputs have no value", () => {
+    render(<Login />);
+
+    const button = screen.getByRole("button", {
+      name: /Log in/,
+    });
+
+    const email = screen.getByRole("textbox", {
+      name: /Email/,
+    });
+
+    const password = screen.getByLabelText(/Password/);
+
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+
+    fireEvent.change(email, { target: { value: "testEmail" } });
+    expect(button).toBeDisabled();
+
+    fireEvent.change(password, { target: { value: "testPassword" } });
+    expect(button).toBeEnabled();
+  });
+});
+
+export {};
