a015b879a0233d6f48648dfe7549416ef200ffd5
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-27 12:07:54 +0100

    Add global state

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,10 +1,18 @@
+import { useReducer } from "react";
+import { DEFAULT_STATE } from "./global-state/constants";
+import { Context } from "./global-state/context";
+import { reducer } from "./global-state/main-reducer";
 import Login from "./pages/login/login";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, DEFAULT_STATE);
+
   return (
     <div>
-      <Login />
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
+      <Context.Provider value={[state, dispatch]}>
+        <Login />
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </Context.Provider>
     </div>
   );
 }
---
+++ frontend-react/src/pages/release-toggles/release-toggles.tsx
@@ -0,0 +1,5 @@
+const ReleaseToggles = () => {
+  return <h1>Release toggle</h1>;
+};
+
+export default ReleaseToggles;
