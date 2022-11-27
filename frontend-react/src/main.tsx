af9f004f2a4f4ede119202bee8475ef42bc033e7
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-27 12:59:40 +0100

    Add react router

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import { MantineProvider } from "@mantine/core";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        /** Put your mantine theme override here */
-        colorScheme: "dark",
-      }}
-    >
-      <App />
-    </MantineProvider>
+    <BrowserRouter>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
