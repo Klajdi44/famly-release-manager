11f9c780cedf01f7dbc1f1a3354a5015184af015
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-11-29 16:23:48 +0100

    Add fetch hook

---
+++ frontend/src/hooks/use-fetch/use-fetch.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+const useFetch = <T,>(url: string) => {
+  const [error, setError] = useState<null | string>(null);
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    (async () => {
+      try {
+        const request = await fetch(url, { signal: abortController.signal });
+
+        if (!request.ok) {
+          setIsLoading(false);
+          throw new Error("Something went wrong while fetching data");
+        }
+
+        const data = await request.json();
+
+        if (data) {
+          setError(null);
+          setIsLoading(false);
+          setData(data);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.name === "AbortError") {
+            console.log("fetch canceled");
+          } else {
+            console.log("Unexpected error", error);
+            setIsLoading(false);
+            setError(error.message);
+          }
+        }
+      }
+    })();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+export { useFetch };
