4bff26cef4b406ab7c86acd59cbdfa48b04d8807
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-12-08 02:10:27 +0100

    Add login functionality to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           colorScheme: "dark",
         }}
       >
-        <App />
+        <NotificationsProvider>
+          <App />
+        </NotificationsProvider>
       </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
