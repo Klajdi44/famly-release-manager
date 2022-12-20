9c54db94e204a2416d24c31b61992d59286c41f9
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-12-20 14:25:52 +0100

    Add fully working rules

--- backend/src/controllers/prisma.ts
+++ backend/src/controllers/prisma.ts
@@ -14,15 +14,13 @@ type Rule = {
   values: { name: string; id: number }[];
 };
 
-// create a function that takes an array of rulesObject
-// it needs to check the length, if > 0 then a rule is already there,
-//  in order to use AND then we need to check attribute, if they are all the same, no need to use AND, if not then we need to use AND
-//  if rule is already there then we need to check for the rule operator,
-//  if operator is of type isOneOf then OR
-//  if operator is of type isNotOneOf then NOT
-
-const transformFrontendRulesToPrismaRules = (frontendRules: any[]) =>
-  frontendRules.reduce(
+/**
+ * It takes an array of rules and returns an object with two keys: OR, NOT. Each key has an
+ * array of objects as its value
+ * @param {any[]} DBRules - The rules that are coming from the database.
+ */
+const transformDBRulesToPrismaRules = (DBRules: any[]) => {
+  return DBRules.reduce(
     (acc, currentRule: Rule) => {
       if (currentRule.operator === "isOneOf") {
         if (currentRule.attribute === "Country") {
@@ -61,17 +59,8 @@ const transformFrontendRulesToPrismaRules = (frontendRules: any[]) =>
     {
       OR: [],
       NOT: [],
-      AND: [],
     }
   );
-
-const transformDBRulesToPrismaRules = (
-  DBRules: any[],
-  frontendRules: any[]
-) => {
-  // if (DBRules.length === 0) {
-  return transformFrontendRulesToPrismaRules(frontendRules);
-  // }
 };
 
 export const test = async (req: Request, res: Response) => {
@@ -87,11 +76,20 @@ export const test = async (req: Request, res: Response) => {
     };
 
     const rulesObject1 = {
-      attribute: "Country",
+      attribute: "Subscription",
       operator: "isNotOneOf",
-      id: "1",
-      values: [{ name: "Austria", id: 7 }],
+      id: "2",
+      values: [{ title: "Starter", id: 2 }],
     };
+
+    const rulesFromFrontend = [
+      {
+        attribute: "Country",
+        operator: "isOneOf",
+        id: "1",
+        values: [{ name: "Austria", id: 7 }],
+      },
+    ];
 
     const segmentFromDb = await prisma.segment.findUnique({
       where: {
@@ -99,7 +97,7 @@ export const test = async (req: Request, res: Response) => {
       },
     });
 
-    const rulesFromDb = segmentFromDb?.rules ?? [];
+    // const rulesFromDb = segmentFromDb?.rules ?? [];
 
     if (
       segmentFromDb?.rules &&
@@ -107,11 +105,24 @@ export const test = async (req: Request, res: Response) => {
       Array.isArray(segmentFromDb?.rules)
     ) {
       const segmentRules = segmentFromDb?.rules as Prisma.JsonArray;
-      // const prismaRules = transformDBRulesToPrismaRules(segmentRules);
-      const prismaRules = transformDBRulesToPrismaRules(
-        [],
-        [rulesObject, rulesObject1]
-      );
+
+      const mergeFrontendSentRulesWithDbRules = [
+        ...segmentRules,
+        ...rulesFromFrontend,
+      ];
+
+      const segmentWithUpdatedRules = await prisma.segment.update({
+        where: {
+          id: 2,
+        },
+        data: {
+          rules: mergeFrontendSentRulesWithDbRules,
+        },
+      });
+
+      // console.log({ segmentWithUpdatedRules });
+
+      const prismaRules = transformDBRulesToPrismaRules([]);
 
       const sites = await prisma.site.findMany({
         where: prismaRules,
@@ -121,39 +132,25 @@ export const test = async (req: Request, res: Response) => {
       });
 
       // update segment sites
+
       const newSites = await prisma.segment.update({
         where: {
-          id: 1,
+          id: 2,
         },
         data: {
           sites: {
             set: sites,
           },
-          rules: [rulesObject, rulesObject1],
+        },
+        include: {
+          sites: true,
         },
       });
       console.log({ newSites });
 
-      res.send(newSites);
+      res.send(segmentWithUpdatedRules);
     }
 
     res.send([]);
   } catch {}
 };
-
-// ****** STEP 1: Query the sites, that should be part of the segment.
-//      - Use prisma.model.findMany()
-//      - Rules are added in the where object:
-//      - Accepts following operators: OR, AND, NOT
-//      - Select only id.
-//      - The return value from sites will be an array of objects with ids, like this:
-//        [ { id: 23 }, { id: 37 }, { id: 43 }, { id: 57 }, { id: 93 } ]
-// *
-
-// ****** STEP 2: Update segment - with the sites returned from STEP 1.
-//      - Use prisma.model.update()
-//      - Select the segment to update in the where object, based on id
-//      - Add data object to the update method.
-//      - Specify sites as the property to update.
-//      - Use set and add the sites from STEP 1.
-//      - Prisma handles the many-to-many relation and updates the relation_table "_SegmentToSite"
