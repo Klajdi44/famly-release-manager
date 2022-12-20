77604b557dc78a481bb91e6a1db827c1f938aa0b
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-12-20 11:12:02 +0100

    Start work with new rule approach in the backend

--- backend/src/controllers/prisma.ts
+++ backend/src/controllers/prisma.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from "express";
 import { prisma } from "../prisma";
 
+type Attribute = "Country" | "Subscription" | "SiteId";
+
+type Operator = "isOneOf" | "isNotOneOf";
+
+type Rule = {
+  attribute: Attribute;
+  operator: Operator;
+  id: string;
+  values: { name: string; id: number }[];
+};
+
+const rule: Rule = {
+  attribute: "Country",
+  operator: "isOneOf",
+  id: "1",
+  values: [
+    { name: "Denmark", id: 1 },
+    { name: "USA", id: 3 },
+  ],
+};
+
 export const test = async (req: Request, res: Response) => {
-  console.log("** running prisma test");
   try {
+    const sites = await prisma.site.findMany({
+      where: {
+        OR: [{ countryId: 1 }, { countryId: 3 }],
+      },
+    });
     // Adding the sites that belongs to a segment:
 
     // ****** STEP 1: Query the sites, that should be part of the segment.
@@ -59,3 +84,62 @@ export const test = async (req: Request, res: Response) => {
     return res.status(500).json(error);
   }
 };
+
+// export const test = async (req: Request, res: Response) => {
+//   console.log("** running prisma test");
+//   try {
+//     // Adding the sites that belongs to a segment:
+
+//     // ****** STEP 1: Query the sites, that should be part of the segment.
+//     //      - Use prisma.model.findMany()
+//     //      - Rules are added in the where object:
+//     //      - Accepts following operators: OR, AND, NOT
+//     //      - Select only id.
+//     //      - The return value from sites will be an array of objects with ids, like this:
+//     //        [ { id: 23 }, { id: 37 }, { id: 43 }, { id: 57 }, { id: 93 } ]
+//     // *
+
+//     const sites = await prisma.site.findMany({
+//       where: {
+//         OR: [{ countryId: 1 }, { countryId: 2 }],
+//         AND: [
+//           { subscriptionId: 3 },
+//           // {subscriptionId: 4},
+//         ],
+//         NOT: [{ name: "Site #1" }],
+//       },
+//       select: {
+//         id: true,
+//       },
+//     });
+
+//     console.log(sites);
+//     // ****** STEP 2: Update segment - with the sites returned from STEP 1.
+//     //      - Use prisma.model.update()
+//     //      - Select the segment to update in the where object, based on id
+//     //      - Add data object to the update method.
+//     //      - Specify sites as the property to update.
+//     //      - Use set and add the sites from STEP 1.
+//     //      - Prisma handles the many-to-many relation and updates the relation_table "_SegmentToSite"
+
+//     const segmentUpdate = await prisma.segment.update({
+//       where: {
+//         id: 1,
+//       },
+//       data: {
+//         sites: {
+//           set: sites,
+//         },
+//       },
+//       include: {
+//         sites: true,
+//       },
+//     });
+
+//     console.log("** segment.update ", segmentUpdate);
+
+//     return res.json(segmentUpdate);
+//   } catch (error) {
+//     return res.status(500).json(error);
+//   }
+// };
