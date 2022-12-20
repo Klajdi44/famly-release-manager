bf9d9cb0da5e8248e081425ccf38aca8fe810c47
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2022-12-20 13:44:21 +0100

    Add working version of rules when no existing rules are in DB

--- backend/src/controllers/prisma.ts
+++ backend/src/controllers/prisma.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { where } from "sequelize";
 import { prisma } from "../prisma";
+import { Prisma } from "@prisma/client";
 
 type Attribute = "Country" | "Subscription" | "SiteId";
 
@@ -12,24 +14,68 @@ type Rule = {
   values: { name: string; id: number }[];
 };
 
-// const rulesFromDb = [rule, rule, rule];
+// create a function that takes an array of rulesObject
+// it needs to check the length, if > 0 then a rule is already there,
+//  in order to use AND then we need to check attribute, if they are all the same, no need to use AND, if not then we need to use AND
+//  if rule is already there then we need to check for the rule operator,
+//  if operator is of type isOneOf then OR
+//  if operator is of type isNotOneOf then NOT
 
-// const transformer(rulesFromDb){
-//   returns {
-//     OR: [{ countryId: 1 }, { countryId: 3 }],
-//     AND:[],
-//     NOT:[]
-//   }
-// }
+const transformFrontendRulesToPrismaRules = (frontendRules: any[]) =>
+  frontendRules.reduce(
+    (acc, currentRule: Rule) => {
+      if (currentRule.operator === "isOneOf") {
+        if (currentRule.attribute === "Country") {
+          const countryIds = currentRule.values.map(country => ({
+            countryId: country.id,
+          }));
+
+          acc["OR"].push(...countryIds);
+        }
+        if (currentRule.attribute === "Subscription") {
+          const subscriptionIds = currentRule.values.map(subscription => ({
+            subscriptionId: subscription.id,
+          }));
+          acc["OR"].push(...subscriptionIds);
+        }
+      }
+
+      if (currentRule.operator === "isNotOneOf") {
+        if (currentRule.attribute === "Country") {
+          const countryIds = currentRule.values.map(country => ({
+            countryId: country.id,
+          }));
+
+          acc["NOT"].push(...countryIds);
+        }
+        if (currentRule.attribute === "Subscription") {
+          const subscriptionIds = currentRule.values.map(subscription => ({
+            subscriptionId: subscription.id,
+          }));
+          acc["NOT"].push(...subscriptionIds);
+        }
+      }
+
+      return acc;
+    },
+    {
+      OR: [],
+      NOT: [],
+      AND: [],
+    }
+  );
+
+const transformDBRulesToPrismaRules = (
+  DBRules: any[],
+  frontendRules: any[]
+) => {
+  // if (DBRules.length === 0) {
+  return transformFrontendRulesToPrismaRules(frontendRules);
+  // }
+};
 
 export const test = async (req: Request, res: Response) => {
   try {
-    // const sitess = await prisma.site.findMany({
-    //   where: {
-    //     OR: [{ countryId: 1 }, { countryId: 3 }],
-    //   },
-    // });
-
     const rulesObject = {
       attribute: "Country",
       operator: "isOneOf",
@@ -40,12 +86,58 @@ export const test = async (req: Request, res: Response) => {
       ],
     };
 
-    const rulesFromDb = await prisma.segment.update({
+    const rulesObject1 = {
+      attribute: "Country",
+      operator: "isNotOneOf",
+      id: "1",
+      values: [{ name: "Austria", id: 7 }],
+    };
+
+    const segmentFromDb = await prisma.segment.findUnique({
       where: {
         id: 1,
       },
-      data: {},
     });
+
+    const rulesFromDb = segmentFromDb?.rules ?? [];
+
+    if (
+      segmentFromDb?.rules &&
+      typeof segmentFromDb?.rules === "object" &&
+      Array.isArray(segmentFromDb?.rules)
+    ) {
+      const segmentRules = segmentFromDb?.rules as Prisma.JsonArray;
+      // const prismaRules = transformDBRulesToPrismaRules(segmentRules);
+      const prismaRules = transformDBRulesToPrismaRules(
+        [],
+        [rulesObject, rulesObject1]
+      );
+
+      const sites = await prisma.site.findMany({
+        where: prismaRules,
+        select: {
+          id: true,
+        },
+      });
+
+      // update segment sites
+      const newSites = await prisma.segment.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          sites: {
+            set: sites,
+          },
+          rules: [rulesObject, rulesObject1],
+        },
+      });
+      console.log({ newSites });
+
+      res.send(newSites);
+    }
+
+    res.send([]);
   } catch {}
 };
 
