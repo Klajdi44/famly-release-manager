bf8250a83d186fb0d1464791e0d3f733472ee07c
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2023-01-04 16:25:36 +0100

    Change controller to shceduleRelease

--- backend/src/routes/scheduleRelease.ts
+++ backend/src/routes/scheduleRelease.ts
@@ -2,7 +2,7 @@ import express from "express";
 import {
   scheduleReleaseToggle,
   deleteScheduleForReleaseToggle,
-} from "../controllers/prisma";
+} from "../controllers/scheduleRelease";
 
 const router = express.Router();
 
